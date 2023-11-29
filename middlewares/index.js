0b49d0370a79dbb0b982a5d43c982748468e3fc1
ilesyk <pavloyavorskyi@ukr.net>
2023-11-29 14:01:36 +0200

    hw5 v1

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,3 +2,4 @@ export {validateBody} from "./validateBody.js";
 export {isValidId} from "./isValidId.js";
 export { isEmptyBody } from "./isEmptyBody.js";
 export { default as authenticate } from "./authenticate.js";
+export {default as upload} from "./upload.js"
